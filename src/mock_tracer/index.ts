3e31e2711603fca0ce1f494b381eb3fe8ec87b1a
Ibnu Yahya <anak10thn@gmail.com>
2021-06-25 01:27:22 +0700

    Update index.ts

--- src/mock_tracer/index.ts
+++ src/mock_tracer/index.ts
@@ -1,6 +1,6 @@
-import MockContext from './mock_context';
-import MockSpan from './mock_span';
-import MockTracer from './mock_tracer';
+import MockContext from './mock_context.ts';
+import MockSpan from './mock_span.ts';
+import MockTracer from './mock_tracer.ts';
 
 export {
     MockTracer,
